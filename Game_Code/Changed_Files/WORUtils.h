3c5f78c0ce25811d6dadad49c55b908453c464f1
ShadowShell <cloudcodexmain@gmail.com>
2015-03-04 16:58:18 +0100

    UTILITY File

---
+++ Game_Code/Changed_Files/WORUtils.h
@@ -0,0 +1,86 @@
+
+#ifndef __WOR_UTILITY_H_INCLUDED__ // If not defined
+#define __WOR_UTILITY_H_INCLUDED__ // Define it
+
+
+//using namespace std;
+//
+#include<string>
+#include<fstream>
+#include<iostream>
+#include<windows.h>
+
+#include <string.h>
+#include <wchar.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+
+
+//#include "StdAfx.h"
+
+
+inline void DebuggerLogThis( std::string IN_TO_BE_LOGGED )
+{
+	
+	// IN_TO_BE_LOGGED = is the string appended on a new line to the file
+
+	std::string OUT_FILE = "DebugLog.txt";
+
+	SYSTEMTIME time;
+	GetSystemTime(&time);
+	std::ofstream DebugFile;
+
+	DebugFile.open (OUT_FILE, std::ios_base::app);
+	// time managment
+	DebugFile << 
+	"|" << std::to_string(time.wMilliseconds) << ":" <<
+	""  << std::to_string(time.wSecond      ) << ":" <<
+	""  << std::to_string(time.wMinute      ) << ":" <<
+	""  << std::to_string(time.wHour        ) << "|" <<
+	""  << std::to_string(time.wDay         ) << ":" <<
+	""  << std::to_string(time.wMonth       ) << ":" <<
+	""  << std::to_string(time.wYear        ) << "|" <<
+	" Running " << IN_TO_BE_LOGGED  << "\n";
+	DebugFile.close();
+}
+
+inline float GetExposed( int IN_LINE_NUMBER )
+{
+	// IN_LINE_NUMBER = the line number of the txtfile to pass to a number, it will pass the first 10 characters
+
+	std::ifstream infile;
+	std::string importFilePath;
+	importFilePath = "Exposed.txt";
+	infile.open(importFilePath); 
+
+	std::string line;
+	float exposedVar;
+	std::string newString;
+
+	int l = 0;
+
+	while ( getline(infile, line) ) // runs through the file line by line
+	{
+		// if the line select is equal to the line we want to get
+		if (l == IN_LINE_NUMBER)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				newString = newString + line[i]; // construct string to be passed
+			}
+		}
+
+		l++;
+	}
+
+	infile.close();
+	exposedVar = std::stof( newString );
+	//float exposedVar = 0;
+	return exposedVar;
+}
+
+#endif//__WOR_UTILITY_H_INCLUDED__ // End define
+
+
